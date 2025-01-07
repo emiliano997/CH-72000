b7b3c2bed49eebbbd40871666e0bcdc6fa1dff14
Emiliano Perez <emi.perez997@gmail.com>
2025-01-07 09:36:11 -0300

    [ADD] Clase 0

---
+++ clase_0/src/PrimerPrograma.java
@@ -0,0 +1,12 @@
+public class PrimerPrograma {
+
+  // Este metodo me permite ejecutar el programa
+  public static void main(String[] args) {
+    System.out.println("Nombre: Emiliano");
+    System.out.println("Apellido: Perez");
+
+    // Ejercicio: Escribir un programa que imprima tus datos
+    // Por ejemplo: nombre, apellido, edad, direccion, etc.
+
+  }
+}
